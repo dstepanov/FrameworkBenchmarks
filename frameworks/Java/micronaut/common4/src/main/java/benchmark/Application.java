5cde814a48aea772e61557fc67d6b9c5b4980693
Denis Stepanov <denis.s.stepanov@oracle.com>
2023-04-26 12:50:32 -0500

    Progress15

--- frameworks/Java/micronaut/common4/src/main/java/benchmark/Application.java
+++ frameworks/Java/micronaut/common4/src/main/java/benchmark/Application.java
@@ -5,6 +5,7 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(String[] args) {
+        System.setProperty("reactor.schedulers.defaultPoolSize", "512");
         Micronaut.build(args).environments("common").classes(Application.class).start();
     }
 
